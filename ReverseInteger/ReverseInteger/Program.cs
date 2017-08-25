9b83344bc79ce030e764018b6f40e5fa5c0306b8
Jc Feng <jcfeng2013@gmail.com>
2017-08-26 07:32:33 +0800

    Reverse Integer

---
+++ ReverseInteger/ReverseInteger/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Reverse(154511515));
+            Console.ReadLine();
+        }
+
+        public static int Reverse(int x)
+        {
+            long sum = 0;
+            while (x != 0)
+            {
+                sum = sum * 10 + x % 10;
+                x /= 10;
+            }
+            return (sum > int.MaxValue || sum < int.MinValue) ? 0 : (int)sum;
+        }
+    }
+}
