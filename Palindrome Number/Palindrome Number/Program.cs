94c8fb7e479221c603c942e3bb025606a080634d
Jc Feng <jcfeng2013@gmail.com>
2017-08-27 07:57:17 +0800

    Palindrome Number
    逐字比对，效率较低，可以添加负数判断，或将数字翻转后比对。

---
+++ Palindrome Number/Palindrome Number/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.IsPalindrome(157848751));
+            Console.ReadLine();
+        }
+    }
+    public class Solution
+    {
+        public  bool IsPalindrome(int x)
+        {
+            string s = Convert.ToString(x);
+            for (int i = 0; i < s.Length / 2; i++)
+                if (s[i] != s[s.Length - i - 1])
+                    return false;
+            return true;
+
+        }
+    }
+}
