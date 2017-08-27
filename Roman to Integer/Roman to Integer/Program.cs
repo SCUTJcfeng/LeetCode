2ee26c994964c8dd2138aec65a104260501004df
Jc Feng <jcfeng2013@gmail.com>
2017-08-28 07:26:11 +0800

    Roman to Integer
    Dictionary

---
+++ Roman to Integer/Roman to Integer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roman_to_Integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.RomanToInt("MDCCCLXXXIV"));
+            Console.ReadLine();
+        }
+    }
+    public class Solution
+    {
+        public int RomanToInt(string s)
+        {
+            Dictionary<string, int> dt = new Dictionary<string, int>();
+
+            dt.Add("I", 1);
+            dt.Add("X", 10);
+            dt.Add("C", 100);
+            dt.Add("M", 1000);
+            dt.Add("V", 5);
+            dt.Add("L", 50);
+            dt.Add("D", 500);
+
+            char[] chr = s.ToCharArray();
+            int sum = 0;
+            for (int i = 0; i < chr.Length; i++)
+            {
+                if (i + 1 <= chr.Length - 1 && dt[chr[i].ToString()] < dt[chr[i + 1].ToString()])
+                {
+                    sum += dt[chr[i + 1].ToString()] - dt[chr[i].ToString()];
+                    i++;
+                }
+                else sum += dt[chr[i].ToString()];
+            }
+            return sum;
+        }
+    }
+}
