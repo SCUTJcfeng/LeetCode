74df30f45c3d3a44effa94eceb24ade9f885316a
Jc Feng <jcfeng2013@gmail.com>
2017-08-30 07:18:14 +0800

    Valid Parentheses
    Stack

---
+++ Valid Parentheses/Valid Parentheses/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valid_Parentheses
+{
+    /*
+    Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+    The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+    */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.IsValid("){"));
+            Console.ReadLine();
+        }
+    }
+
+    public class Solution
+    {
+        public bool IsValid(string s)
+        {
+            if (s.Length == 0 || s.Length % 2 == 1)
+                return false;
+            Stack<char> stack = new Stack<char>();
+            foreach (char c in s)
+            {
+                if (c == '(' || c == '[' || c == '{')
+                    stack.Push(c);
+                if (c == ')' || c == ']' || c == '}')
+                {
+                    if (stack.Count == 0)
+                        return false;
+                    if (c == ')' && stack.Peek() != '(')
+                        return false;
+                    if (c == ']' && stack.Peek() != '[')
+                        return false;
+                    if (c == '}' && stack.Peek() != '{')
+                        return false;
+                    stack.Pop();
+                }
+            }
+            if (stack.Count == 0)
+                return true;
+            return false;
+        }
+    }
+}
