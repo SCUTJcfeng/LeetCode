e52593887683b424a0369e44aa098ea4715be519
Jc Feng <jcfeng2013@gmail.com>
2017-09-01 07:27:59 +0800

    Add files via upload

---
+++ Merge-Two-Sorted-Lists/Merge-Two-Sorted-Lists/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merge_Two_Sorted_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+    public class Solution
+    {
+        public ListNode MergeTwoLists(ListNode head1, ListNode head2)
+        {
+            ListNode fakeHead = new ListNode(0);
+            fakeHead.next = head1;
+            ListNode pre = fakeHead;
+            while (head1 != null && head2 != null)
+            {
+                if (head1.val < head2.val)
+                {
+                    head1 = head1.next;
+                }
+                else
+                {
+                    ListNode next = head2.next;
+                    head2.next = pre.next;
+                    pre.next = head2;
+                    head2 = next;
+                }
+                pre = pre.next;
+            }
+            if (head2 != null)
+            {
+                pre.next = head2;
+            }
+            return fakeHead.next;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+}
---
+++ Remove Duplicates from Sorted Array/Remove Duplicates from Sorted Array/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remove_Duplicates_from_Sorted_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4, 4, 5 };
+            Solution solution = new Solution();
+            Console.WriteLine(solution.RemoveDuplicates(nums));
+            Console.ReadLine();
+        }
+    }
+
+    public class Solution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            int temp = 0;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[temp] != nums[i])
+                {
+                    temp++;
+                    nums[temp] = nums[i];
+                }
+            }
+            return temp + 1;
+        }
+    }
+}
