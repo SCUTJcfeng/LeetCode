bb3464e3216d0593b941b21ee133c6102f789ef4
Jc Feng <jcfeng2013@gmail.com>
2017-09-02 06:57:18 +0800

    Add files via upload

---
+++ Remove Element/Remove Element/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+ * Given an array and a value, remove all instances of that value in place and return the new length.
+
+Do not allocate extra space for another array, you must do this in place with constant memory.
+
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+Example:
+Given input array nums = [3,2,2,3], val = 3
+
+Your function should return length = 2, with the first two elements of nums being 2.
+
+*/
+
+namespace Remove_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] nums = { 1, 3, 2, 3, 2, 3, 1 };
+            Console.WriteLine(solution.RemoveElement(nums, 3));
+            Console.ReadLine();
+        }
+    }
+
+    public class Solution
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            if (nums.Length == 0)
+                return 0;
+            int i = 0;
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (nums[j] != val)
+                {
+                    nums[i] = nums[j];
+                    i++;
+                }
+            }
+            return i;
+        }
+    }
+}
