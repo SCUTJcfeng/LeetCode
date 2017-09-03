613a974e59b99dee87a7cb008df03eef9b765042
Jc Feng <jcfeng2013@gmail.com>
2017-09-04 07:40:14 +0800

    Add files via upload

---
+++ Add Digits/Add Digits/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+For example:
+
+Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+
+Follow up:
+Could you do it without any loop/recursion in O(1) runtime?
+*/
+
+namespace Add_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.AddDigits(124548));
+            Console.ReadLine();
+        }
+    }
+
+    public class Solution
+    {
+        public int AddDigits(int num)
+        {
+            if (num < 10)
+                return num;
+
+            while (true)
+            {
+                num = Add(num);
+                if (num < 10)
+                    return num;
+            }
+        }
+        public int Add(int num)
+        {
+            int sum = 0;
+
+            while (num != 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+            return sum;
+        }
+    }
+}
